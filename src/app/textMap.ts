38b554a9f0cfe88d8c611cf2885f52055a4ef34f
Andrea <andreatommasini1998+dev@gmail.com>
2026-06-22 10:21:02 +0200

    fix textMap.ts

--- src/app/textMap.ts
+++ src/app/textMap.ts
@@ -1,6 +1,6 @@
 export const textMap = {
   home: {
-    text: "<strong>Petit</strong> è un centro polispecialistico dell'età evolutiva specializzato nell'abilitazione e <strong>riabilitazione dei disturbi del neurosviluppo</strong>.<br> Offriamo una <strong>presa in carico terapeutica globale</strong> e grazie a un approccio multidisciplinare e a differenti metodologie riabilitative, seguiamo ogni paziente con la massima cura per garantirne il benessere e l'evoluzione."
+    text: "<strong>Petit</strong> è un centro polispecialistico dell'età evolutiva specializzato nella <strong>riabilitazione dei disturbi del neurosviluppo</strong>.<br> Offriamo una <strong>presa in carico terapeutica globale</strong> e grazie a un approccio multidisciplinare e a differenti metodologie riabilitative, seguiamo ogni paziente con la massima cura per garantirne il benessere e l'evoluzione."
   },
   services: [
     {
